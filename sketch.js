d7fbbf5b2406063c73b59179302358a483d22113
NatG91 <68413475+NatG91@users.noreply.github.com>
2020-08-05 11:07:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var hypnoticBall;
+var database;
+var Position;
+
+function setup(){
+database = firebase.database();
+console.log(database);
+    createCanvas(500,500);
+    hypnoticBall = createSprite(250,250,10,10);
+    hypnoticBall.shapeColor = "red"
+    var hypnoticBallPosition = database.ref('Ball/Position');
+    hypnoticBallPosition.on("value",readPosition,showError);
+}
+        
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+function writePosition(x,y){
+    database.ref('Ball/Position').set({
+        'x' : Position.x + x,
+        'y': Position.y + y
+        
+    })
+}
+function readPosition(data){
+    Position=data.val();
+
+    console.log("Nathan")
+    hypnoticBall.x = Position.x 
+    hypnoticBall.y = Position.y
+}
+
+    function showError(){
+        console.log("Error")
+    }
+
